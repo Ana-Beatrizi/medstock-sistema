be374e980eaecd49312dba34ac196817ec116509
Rumi <243357032+Ana-Beatrizi@users.noreply.github.com>
2026-04-14 09:52:45 -0300

    limpei

--- README.md
+++
@@ -1,2 +0,0 @@
-# medstock_individual
-
--- bd_config.py
+++
@@ -1,7 +0,0 @@
-DB_CONFIG = {
-    "host": "localhost",
-    "user": "root",
-    "password": "123456",
-    "database": "Medstock_db",
-    "auth_plugin": "mysql_native_password" 
-}
